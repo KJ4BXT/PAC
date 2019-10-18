b281e903abbb7f5634d8232bc1f6530db3d34718
Taylor Delawter Reuter <taylorkleinr@gmail.com>
2019-10-18 14:39:13 -0400

    Update GUItrial.py
    Added delete ('del') button but it needs to be programmed to actually delete a character or command.

--- GUItrial.py
+++ GUItrial.py
@@ -79,7 +79,7 @@ bottomframe = LabelFrame(Cframe, text=" keypad ", bd=3)
 bottomframe.pack(padx=15, pady=10)
 # typical calculator button layout
 btn_list = [
-['+', '-', '*', '/', '^', '%', '(', ')', '='],
+['+', '-', '*', '/', '^', '%', '(', ')', '=', 'del'],
 ['1',  '2',  '3',  '4',  '5', '6', '7', '8', '9', '0', '-', '+'],
 ['Q',  'W',  'E',  'R',  'T', 'Y', 'U', 'I', 'O', 'P', '[', ']'],
 ['A',  'S',  'D',  'F',  'G', 'H', 'J', 'K', 'L', ';', '"'],
